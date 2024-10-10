341b03dc3d697363469a47cb0ca24538d85e52e3
Satyam-2022 <satyam.chitade@moderncoe.edu.in>
2024-10-10 12:57:32 +0000

    Added lambook and new template

--- src/main/java/com/project/sms/entity/Student.java
+++ src/main/java/com/project/sms/entity/Student.java
@@ -1,9 +1,18 @@
 package com.project.sms.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "students")
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -13,44 +22,4 @@ public class Student {
     private String lastName;
     private String email;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Student(String firstName, String lastName, String email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-    }
-
-    public Student() {
-    }
 }
