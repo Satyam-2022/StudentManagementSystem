64999a54ab2f2ffaf80449e49e44f22ed245d1b8
Satyam-2022 <91516708+Satyam-2022@users.noreply.github.com>
2024-10-22 14:40:13 +0000

    Update Student.java

--- src/main/java/com/project/sms/entity/Student.java
+++ src/main/java/com/project/sms/entity/Student.java
@@ -6,6 +6,13 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
 
 @Entity
 @Table(name = "students")
